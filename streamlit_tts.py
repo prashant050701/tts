3db2fac43564318e71e0d5aad7b226f712cb15ac
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2023-10-27 17:44:13 +0200

    Update streamlit_tts.py

--- streamlit_tts.py
+++ streamlit_tts.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from moviepy.editor import *
-from pydub import AudioSegment
 import tempfile
 
 # Streamlit app title for the new feature
@@ -21,24 +20,19 @@ if video_file:
     audio = video_clip.audio
     audio_filename = tempfile.mktemp(suffix=".mp3")
     audio.write_audiofile(audio_filename)
-
-    # Display audio player and download link
-    st.audio(audio_filename, format='audio/mp3')
-    st.markdown(f'<a href="{audio_filename}" download>Download Audio</a>', unsafe_allow_html=True)
-
-    # Display audio waveform and selection bar
-    audio_segment = AudioSegment.from_file(audio_filename)
-    waveform = st.audio_waveform(audio_segment)  # Note: st.audio_waveform is fictional, replace with actual waveform rendering
+    
+    # Now we'll handle the audio segment selection and export
+    # Use a slider to allow the user to select a range of the audio to export
     selection = st.slider(
         "Select a range (in seconds):",
-        0, len(audio_segment) // 1000,
-        (0, len(audio_segment) // 1000)
+        0, int(video_clip.duration),
+        (0, int(video_clip.duration))
     )
 
     # Export selected audio range
     if st.button("Export Selected Range"):
         start_time, end_time = selection
-        selected_segment = audio_segment[start_time*1000:end_time*1000]
+        selected_segment = audio.subclip(start_time, end_time)
         selected_audio_filename = tempfile.mktemp(suffix=".mp3")
-        selected_segment.export(selected_audio_filename, format="mp3")
+        selected_segment.write_audiofile(selected_audio_filename)
         st.markdown(f'<a href="{selected_audio_filename}" download>Download Selected Range</a>', unsafe_allow_html=True)
