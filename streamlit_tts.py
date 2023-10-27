f7ae4392781ce5622b2a4bb2f2658dcf24ea07f9
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2023-10-27 17:32:37 +0200

    Update streamlit_tts.py

--- streamlit_tts.py
+++ streamlit_tts.py
@@ -1,39 +1,40 @@
 import streamlit as st
-from gtts import gTTS
-import io
-import base64
+from moviepy.editor import *
+from pydub import AudioSegment
+import tempfile
 
-# Streamlit app title
-st.title("Text-to-Speech Converter")
+# Streamlit app title for the new feature
+st.title("Video to Audio Extractor")
 
-# Text input
-text = st.text_area("Enter the text you want to convert to audio:")
+# Video file upload
+video_file = st.file_uploader("Upload a video file:", type=['mp4', 'mkv', 'avi'])
 
-# Convert button
-if st.button("Convert to Audio"):
-    if text:
-        # Generate audio
-        tts = gTTS(text, lang='en', tld='co.in')
-        audio_bytes = io.BytesIO()
-        tts.write_to_fp(audio_bytes)
+if video_file:
+    # Load the video file
+    video_clip = VideoFileClip(video_file)
+    
+    # Convert video to audio
+    audio = video_clip.audio
+    audio_filename = tempfile.mktemp(suffix=".mp3")
+    audio.write_audiofile(audio_filename)
 
-        # Save audio to a fixed filename
-        audio_filename = "output.mp3"
-        with open(audio_filename, "wb") as audio_file:
-            audio_file.write(audio_bytes.read())
+    # Display audio player and download link
+    st.audio(audio_filename, format='audio/mp3')
+    st.markdown(f'<a href="{audio_filename}" download>Download Audio</a>', unsafe_allow_html=True)
 
-        audio_b64 = base64.b64encode(audio_bytes.getvalue()).decode()
-        href = f'<a href="data:audio/mp3;base64,{audio_b64}" download="output.mp3">Download Audio</a>'
-        st.markdown(href, unsafe_allow_html=True)
-hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-# Info and instructions
-st.markdown("Instructions:")
-st.markdown("1. Enter the text in the text box.")
-st.markdown("2. Click the 'Convert to Audio' button.")
-st.markdown("3. Download it using the link below.")
+    # Display audio waveform and selection bar
+    audio_segment = AudioSegment.from_file(audio_filename)
+    waveform = st.audio_waveform(audio_segment)
+    selection = st.slider(
+        "Select a range (in seconds):",
+        0, len(audio_segment) // 1000,
+        (0, len(audio_segment) // 1000)
+    )
+
+    # Export selected audio range
+    if st.button("Export Selected Range"):
+        start_time, end_time = selection
+        selected_segment = audio_segment[start_time*1000:end_time*1000]
+        selected_audio_filename = tempfile.mktemp(suffix=".mp3")
+        selected_segment.export(selected_audio_filename, format="mp3")
+        st.markdown(f'<a href="{selected_audio_filename}" download>Download Selected Range</a>', unsafe_allow_html=True)
